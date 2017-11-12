e9ba8f494a1d2cc4c824ca90e13149d3c70ed117
q.marcou <quentin.marcou@ens.fr>
2017-11-12 17:27:07 +0100

    Repair REGEXP expression for automatically building new release

--- build_release.sh
+++ build_release.sh
@@ -2,20 +2,23 @@
 if [ $# == 1 ]; then
 	#Automate release number change in configure.ac
 	DOTTEDVERSION=$(echo $1 | sed s/-/./g) 
-	MYREGEXP='[0-9\.]+'
+	MYREGEXP='[0-9+\.]+'
 	EMAILREGEXP='([[:alnum:]_.-]+@[[:alnum:]_.-]+?\.[[:alpha:].]{2,6})'
-	PREVLINE=$(grep -E 'AC_INIT\(\[igor\]\,\ \['$MYREGEXP'\]\,\ \['$EMAILREGEXP'\]\)' configure.ac)
-	NEWLINE=$(grep -E 'AC_INIT\(\[igor\]\,\ \['$MYREGEXP'\]\,\ \['$EMAILREGEXP'\]\)' configure.ac | sed -r s/$MYREGEXP/$DOTTEDVERSION/)
-	echo $NEWLINE
+	URLREGEXP='(http|https)\://[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(/[a-zA-Z0-9\-\.]*)*'
+	PREVLINE=$(grep -E 'AC_INIT\(\[igor\]\,\ \['$MYREGEXP'\]\,\ \['$EMAILREGEXP'\]\,\ \[igor\]\,\ \['$URLREGEXP'\]\)' configure.ac)
+	NEWLINE=$(grep -E 'AC_INIT\(\[igor\]\,\ \['$MYREGEXP'\]\,\ \['$EMAILREGEXP'\]\,\ \[igor\]\,\ \['$URLREGEXP'\]\)' configure.ac | sed -r s/$MYREGEXP/$DOTTEDVERSION/)
 	#Now replace it in configure.ac
 	#reescape everythin
-	ZZ=$(echo $PREVLINE | sed s/'\['/'\\['/g)
-	ZZ=$(echo $ZZ | sed s/'\]'/'\\]'/g)
-	ZZ=$(echo $ZZ | sed s/'\.'/'\\.'/g)
-	ZZ=$(echo $ZZ | sed 's/)/\\)/g')
-	ZZ=$(echo $ZZ | sed 's/(/\\(/g')
+	ZZ=$(echo $PREVLINE | sed s/'\['/'\\['/g) #escape opening bracket
+	ZZ=$(echo $ZZ | sed s/'\]'/'\\]'/g) #escape closing bracket
+	ZZ=$(echo $ZZ | sed s/'\.'/'\\.'/g) #escape backslash
+	ZZ=$(echo $ZZ | sed 's/)/\\)/g') #escape closing parenthesis
+	ZZ=$(echo $ZZ | sed 's/(/\\(/g') #escape opening parenthesis
+	ZZ=$(echo $ZZ | sed 's/\:/\\:/g') #escape colum
+	ZZ=$(echo $ZZ | sed 's/\ /\\ /g') #escape spaces
+	#ZZ=$(echo $ZZ | sed 's/\//\\//g') #escape forward slashes => removed as changing the delimiters to = in the next line is cleaner
 
-	sed -r "s/$ZZ/$NEWLINE/" configure.ac > tmpfile
+	sed -r "s=$ZZ=$NEWLINE=" configure.ac > tmpfile
 	rm configure.ac
 	mv tmpfile configure.ac
 	
@@ -50,7 +53,7 @@ if [ $# == 1 ]; then
 	
 elif [ $# == 0 ]; then
 	echo No release number has been provided... Leaving without performing any action
-	echo Please provide a release number as argument
+	echo Please provide a release number as argument with dash seperated version number e.g 5-2-3
 else
 	echo Too many arguments were passed
 	echo Please provide only the release number as argument
